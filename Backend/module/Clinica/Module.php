67de7883884793a7e3743249046daa0701fe4a45
AlexandreCaueSouzadaSilva <caue8766@gmail.com>
2025-11-16 12:42:43 -0300

    Escrito codigo no module.php - pra teste apenas

---
+++ Backend/module/Clinica/Module.php
@@ -0,0 +1,9 @@
+<?php
+// Reposabilidade : Definir o módulo Clinica
+nameSpace Clinica;
+
+// Importando interfaces necessárias do Zend Framework
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+// Definindo...
+?>
